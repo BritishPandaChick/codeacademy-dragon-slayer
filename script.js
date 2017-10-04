3e7d7917b96ed559cb56c3e5defca6d289a48e71
BritishPandaChick <sbartley1080@gmail.com>
2017-10-04 18:20:00 -0400

    Add dragon slayer files

---
+++ script.js
@@ -0,0 +1,23 @@
+var slaying = true;
+var youHit = Math.floor(Math.random() * 2);
+var damageThisRound = Math.floor(Math.random() * 5 + 1);
+var totalDamage = 0;
+
+//run game with while loop
+var slaying = true;
+
+while (slaying) {
+  if (youHit) {
+    console.log("Congrats! You hit the dragon.");
+    totalDamage = totalDamage += damageThisRound;
+    if (totalDamage >= 4){
+      console.log("You slayed the dragon!");
+      slaying = false;
+    } else {
+      youHit = Math.floor(Math.random() * 2);
+    }
+  } else {
+    console.log("Sorry. The dragon ate you.");
+  }
+  slaying = false;
+};
